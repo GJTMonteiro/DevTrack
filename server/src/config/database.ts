fce7a1ab3797d10f550f405e8dde6aa31fb97ae1
Guilherme Monteiro <guilhermejtmonteiro2003@gmail.com>
2026-08-18 20:47:43 +0100

    Fix database connection for production

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,12 +1,11 @@
-import "dotenv/config";
-import { Pool } from "pg";
+import 'dotenv/config';
+import { Pool } from 'pg';
 
-const pool = new Pool ({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
